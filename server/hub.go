14a043e702bb42f85e96536eaf3b903f6ccb066e
Ogisan <sachaservanschreiber@gmail.com>
2017-04-16 22:03:26 -0400

    fixed nill pointer dereference

--- server/hub.go
+++ server/hub.go
@@ -52,8 +52,9 @@ func newHub() *Hub {
 
 func (h *Hub) sendMessageToClient(sendToIP string, message []byte) error {
 
-	if client, ok := h.clients[sendToIP]; ok {
+	if _, ok := h.clients[sendToIP]; ok {
 		fmt.Print("Sending data to client")
+		client := h.clients[sendToIP]
 		client.send <- message
 	} else {
 		fmt.Println("\nError: client not connected")
